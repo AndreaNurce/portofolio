da083bbbe78e2c43fd0f3a453af7f6e9761ced2b
Andrea <andrea-dibra@hotmail.com>
2021-03-18 08:42:02 +0100

    pre styling navbar

--- andreanurce/src/components/nav.vue
+++ andreanurce/src/components/nav.vue
@@ -4,13 +4,12 @@
       <div class="nav-bar" >
           <img style="width : 64px;padding:2px;" src="../assets/logo.png" alt="">
           <ul>
-              <a href="#Home"  :class="{active: tag == 'Home' }"  > <li>Home</li></a>
-              <a href="#Service" :class="{active: tag == 'Service' }" > <li>Service</li></a>
-              <a href="#About" :class="{active: tag == 'About' }" > <li>About</li></a>
-              <a href="#Resume" :class="{active: tag == 'Resume' }" > <li>Resume</li></a>
-              <a href="#Works" :class="{active: tag == 'Works' }" > <li>Works</li></a>
-              <a href="#Blog" :class="{active: tag == 'Blog' }" > <li>Blog</li></a>
-              <a href="#Contact"> <li>Contact</li></a>
+              <a href="#Home" @click="scrollToTag('Home') "  :class="{active: tag == 'Home' }"  > <li>Home</li></a>
+              <a href="#Service" @click=" scrollToTag('Service')" :class="{active: tag == 'Service' }" > <li>Service</li></a>
+              <a href="#About"  @click="scrollToTag('About')" :class="{active: tag == 'About' }" > <li>About</li></a>
+              <a href="#Resume"  @click="scrollToTag('Resume')" :class="{active: tag == 'Resume' }" > <li>Resume</li></a>
+              <a href="#Works"  @click="scrollToTag('Works')" :class="{active: tag == 'Works' }" > <li>Works</li></a>
+              <a href="#Contact"  @click="scrollToTag('Contact')" :class="{active: tag == 'Contact' }"  > <li>Contact</li></a>
           </ul>
       </div>
   </div>
@@ -25,6 +24,41 @@ export default {
         }
     },
        methods: {
+        scrollToTag  (el){
+            this.tag = el;
+            if(el == 'Home' ){
+            window.scrollTo({
+            top: 0, 
+            behavior: 'smooth' 
+            })
+           }if(el == 'Service' ){
+            window.scrollTo({
+            top: 750, 
+            behavior: 'smooth' 
+            })
+           }if(el == 'About' ){
+            window.scrollTo({
+            top: 1700, 
+            behavior: 'smooth' 
+            })
+           }if(el == 'Resume' ){
+            window.scrollTo({
+            top: 2510, 
+            behavior: 'smooth' 
+            })
+           }if(el == 'Works' ){
+            window.scrollTo({
+            top: 5423, 
+            behavior: 'smooth' 
+            })
+           }if(el == 'Contact' ){
+            window.scrollTo({
+            top: 6420, 
+            behavior: 'smooth' 
+            })
+           }
+            }
+,
     handleScroll () {
         if(window.pageYOffset > 135){
             document.querySelector('#main-bar').style.position = "fixed";
@@ -34,6 +68,20 @@ export default {
             document.querySelector('#main-bar').style.position = "absolute";
             document.querySelector('#main-bar').style.backgroundColor = "#F9F9FF";
             document.querySelector('#main-bar').style.boxShadow = "none";
+        }
+
+        if(window.pageYOffset < 740){
+            this.tag = 'Home';
+        }if(window.pageYOffset >= 740 && window.pageYOffset < 1700 ){
+            this.tag = 'Service';
+        }if(window.pageYOffset >= 1700 && window.pageYOffset < 2510 ){
+            this.tag = 'About';
+        }if(window.pageYOffset >= 2510 && window.pageYOffset < 5423 ){
+            this.tag = 'Resume';
+        }if(window.pageYOffset >= 5423 && window.pageYOffset < 6420){
+            this.tag = 'Works';
+        }if(window.pageYOffset >= 6100  ){
+            this.tag = 'Contact';
         }
     }
   },mounted () {
@@ -83,6 +131,7 @@ li{
 }
 ul > a {
     padding : 0 5px;
+    outline: none;
 }
 ul > a::before{
 position: absolute;
